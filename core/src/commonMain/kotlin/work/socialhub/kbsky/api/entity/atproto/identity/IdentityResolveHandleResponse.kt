4a9f056042bfc8c7e81b8c22566bb7bd74f7df55
Akihiro Urushiara <a.urusihara@gmail.com>
2024-01-03 01:20:35 +0900

    add objcname annotation test.

--- core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/atproto/identity/IdentityResolveHandleResponse.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/atproto/identity/IdentityResolveHandleResponse.kt
@@ -1,8 +1,12 @@
 package work.socialhub.kbsky.api.entity.atproto.identity
 
 import kotlinx.serialization.Serializable
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
 
 @Serializable
+@OptIn(ExperimentalObjCName::class)
+@ObjCName(swiftName = "KBskyIdentityResolveHandleResponse")
 class IdentityResolveHandleResponse {
     var did: String? = null
 }
