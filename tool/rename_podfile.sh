e3b6a1dcb9ab39a3e54ec019ca22121073f963c0
Akihiro Urushiara <a.urusihara@gmail.com>
2024-01-02 13:13:16 +0900

    change pods name.

--- tool/rename_podfile.sh
+++ tool/rename_podfile.sh
@@ -11,24 +11,24 @@ mkdir -p $BUILD_PATH/cocoapods/repository/release
 # Copy Podspec
 cd "$BASE_PATH" || exit
 cd $BUILD_PATH/cocoapods/publish/debug || exit
-cp pods.podspec ../../repository/pods-debug.podspec
+cp pods.podspec ../../repository/kbsky-debug.podspec
 cd ../../repository/ || exit
-sed -i -e "s|'pods'|'pods-debug'|g" pods-debug.podspec
-sed -i -e "s|'pods.xcframework'|'debug/pods.xcframework'|g" pods-debug.podspec
+sed -i -e "s|'pods'|'kbsky-debug'|g" kbsky-debug.podspec
+sed -i -e "s|'pods.xcframework'|'debug/kbsky.xcframework'|g" kbsky-debug.podspec
 rm *.podspec-e
 cd "$BASE_PATH" || exit
 cd $BUILD_PATH/cocoapods/publish/release || exit
-cp pods.podspec ../../repository/pods-release.podspec
+cp pods.podspec ../../repository/kbsky-release.podspec
 cd ../../repository/ || exit
-sed -i -e "s|'pods'|'pods-release'|g" pods-release.podspec
-sed -i -e "s|'pods.xcframework'|'release/pods.xcframework'|g" pods-release.podspec
+sed -i -e "s|'pods'|'kbsky-release'|g" kbsky-release.podspec
+sed -i -e "s|'pods.xcframework'|'release/kbsky.xcframework'|g" kbsky-release.podspec
 rm *.podspec-e
 
 # Copy Framework
 cd "$BASE_PATH" || exit
 cd $BUILD_PATH/cocoapods/publish/debug || exit
-cp -r pods.xcframework ../../repository/debug/pods.xcframework
+cp -r pods.xcframework ../../repository/debug/kbsky.xcframework
 cd "$BASE_PATH" || exit
 cd $BUILD_PATH/cocoapods/publish/release || exit
-cp -r pods.xcframework ../../repository/release/pods.xcframework
+cp -r pods.xcframework ../../repository/release/kbsky.xcframework
 
