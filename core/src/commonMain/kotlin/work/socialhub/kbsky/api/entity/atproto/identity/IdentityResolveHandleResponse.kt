0b33041a726872b5b860fa6053f459a2eb74b71f
Akihiro Urushiara <a.urusihara@gmail.com>
2024-01-03 17:48:43 +0900

    remove name annotations.

--- core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/atproto/identity/IdentityResolveHandleResponse.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/atproto/identity/IdentityResolveHandleResponse.kt
@@ -1,16 +1,8 @@
 package work.socialhub.kbsky.api.entity.atproto.identity
 
 import kotlinx.serialization.Serializable
-import kotlin.experimental.ExperimentalObjCName
-import kotlin.native.ObjCName
 
 @Serializable
-@OptIn(ExperimentalObjCName::class)
-@ObjCName(
-    name = "KBskyIdentityResolveHandleResponse",
-    swiftName = "KBskyIdentityResolveHandleResponse",
-    exact = true
-)
 class IdentityResolveHandleResponse {
     var did: String? = null
 }
