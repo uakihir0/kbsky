2500a6c121ddb04bd95e6249bda403af61c92a75
Akihiro Urushiara <a.urusihara@gmail.com>
2024-01-19 00:45:01 +0900

    create libs.versions.toml

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,21 +15,19 @@ kotlin {
     macosArm64()
 
     sourceSets {
-        val kotestVersion = "5.8.0"
-
         commonMain.dependencies {
-            implementation("io.ktor:ktor-client-core:2.3.7")
-            implementation("work.socialhub:khttpclient:0.0.1-SNAPSHOT")
-            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+            implementation(libs.ktor.core)
+            implementation(libs.khttpclient)
+            implementation(libs.datetime)
+            implementation(libs.coroutines.core)
+            implementation(libs.serialization.json)
         }
 
         // for test (kotlin/jvm)
         jvmTest.dependencies {
             implementation(kotlin("test"))
-            implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
-            implementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+            implementation(libs.kotest.junit5)
+            implementation(libs.kotest.assertions)
         }
     }
 }
