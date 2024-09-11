4cea4388181395a0450f1eaa0176064bf05e5290
takke <takke30@gmail.com>
2024-09-11 13:03:45 +0900

    Add ATProtocolException.status

--- core/src/commonMain/kotlin/work/socialhub/kbsky/ATProtocolException.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/ATProtocolException.kt
@@ -5,12 +5,13 @@ import work.socialhub.kbsky.api.entity.share.ErrorResponse
 open class ATProtocolException(
     message: String?,
     exception: Exception?,
+    val status: Int?,
 ) : RuntimeException(
     message,
     exception,
 ) {
     var response: ErrorResponse? = null
 
-    constructor(message: String?) : this(message, null)
-    constructor(exception: Exception?) : this(null, exception)
+    constructor(message: String?) : this(message, null, null)
+    constructor(exception: Exception?) : this(null, exception, null)
 }
