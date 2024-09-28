5f2984725f0bc2b0fe215bac4cc4bfa8e6719f5b
takke <takke30@gmail.com>
2024-09-28 18:39:31 +0900

    Add ability to set/unset pinned post to/from profile

--- core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/app/bsky/actor/ActorUpdateProfileRequest.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/app/bsky/actor/ActorUpdateProfileRequest.kt
@@ -1,6 +1,7 @@
 package work.socialhub.kbsky.api.entity.app.bsky.actor
 
 import work.socialhub.kbsky.api.entity.share.AuthRequest
+import work.socialhub.kbsky.model.com.atproto.repo.RepoStrongRef
 import work.socialhub.kbsky.model.share.Blob
 
 class ActorUpdateProfileRequest(
@@ -10,5 +11,8 @@ class ActorUpdateProfileRequest(
     val avatar: Blob? = null,
     val banner: Blob? = null,
     // set true if you want to clear the avatar
-    val clearBanner: Boolean = false
+    val clearBanner: Boolean = false,
+    val pinnedPost: RepoStrongRef? = null,
+    // set true if you want to clear the pinned post
+    val clearPinnedPost: Boolean = false,
 ) : AuthRequest(accessJwt)
--- core/src/commonMain/kotlin/work/socialhub/kbsky/model/app/bsky/actor/ActorProfile.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/model/app/bsky/actor/ActorProfile.kt
@@ -3,6 +3,7 @@ package work.socialhub.kbsky.model.app.bsky.actor
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import work.socialhub.kbsky.BlueskyTypes
+import work.socialhub.kbsky.model.com.atproto.repo.RepoStrongRef
 import work.socialhub.kbsky.model.share.Blob
 import work.socialhub.kbsky.model.share.RecordUnion
 
@@ -20,4 +21,5 @@ class ActorProfile : RecordUnion() {
     var description: String? = null
     var avatar: Blob? = null
     var banner: Blob? = null
+    var pinnedPost: RepoStrongRef? = null
 }
