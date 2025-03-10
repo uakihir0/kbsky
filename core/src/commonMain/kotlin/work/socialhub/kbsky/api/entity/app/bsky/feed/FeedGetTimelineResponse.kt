ebd458b9799d7696851e027e67eac6e95ae45678
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2025-03-11 01:09:02 +0900

    fix FeedGetTimelineResponse import.

--- core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/app/bsky/feed/FeedGetTimelineResponse.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/api/entity/app/bsky/feed/FeedGetTimelineResponse.kt
@@ -1,9 +1,10 @@
 package work.socialhub.kbsky.api.entity.app.bsky.feed
 
 import kotlinx.serialization.Serializable
+import work.socialhub.kbsky.model.app.bsky.feed.FeedDefsFeedViewPost
 
 @Serializable
 data class FeedGetTimelineResponse(
     var cursor: String? = null,
-    var feed: List<work.socialhub.kbsky.model.app.bsky.feed.FeedDefsFeedViewPost>
+    var feed: List<FeedDefsFeedViewPost>
 )
