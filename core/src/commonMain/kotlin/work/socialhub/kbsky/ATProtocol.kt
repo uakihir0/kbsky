6361f4cf8777a3bfdef83c56bfc5e71272545870
takke <takke30@gmail.com>
2025-08-27 16:30:25 +0900

    Add ModerationResource

--- core/src/commonMain/kotlin/work/socialhub/kbsky/ATProtocol.kt
+++ core/src/commonMain/kotlin/work/socialhub/kbsky/ATProtocol.kt
@@ -1,6 +1,7 @@
 package work.socialhub.kbsky
 
 import work.socialhub.kbsky.api.com.atproto.IdentityResource
+import work.socialhub.kbsky.api.com.atproto.ModerationResource
 import work.socialhub.kbsky.api.com.atproto.RepoResource
 import work.socialhub.kbsky.api.com.atproto.ServerResource
 
@@ -8,4 +9,5 @@ interface ATProtocol {
     fun identity(): IdentityResource
     fun server(): ServerResource
     fun repo(): RepoResource
+    fun moderation(): ModerationResource
 }
